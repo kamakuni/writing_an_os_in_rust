94ba5457911905acc5fda5d581504d90ee5aaf1d
sugawara kensuke <superradio86@gmail.com>
2021-05-25 23:08:07 +0900

    Add custom build target settings

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
 
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
-    loop {}
-}
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
