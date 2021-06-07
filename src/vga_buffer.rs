e669c90e9860a57da949cbb2ac14f1812636481d
sugawara kensuke <superradio86@gmail.com>
2021-06-07 22:37:40 +0900

    Add ScreenChar and Buffer structs

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -29,3 +29,14 @@ impl ColorCode {
         ColorCode((background as u8) << 4 | (foreground as u8))
     }
 }
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar: BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
