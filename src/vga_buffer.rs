a086101424cc23c84c2d287c13252dae0543781c
sugawara kensuke <superradio86@gmail.com>
2021-06-18 01:59:55 +0900

    Add static WRITER

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -114,6 +114,12 @@ impl fmt::Write for Writer {
     }
 }
 
+pub static WRITER: Writer = Writer {
+    column_position: 0,
+    color_code: ColorCode::new(Color::Yellow, Color::Black),
+    buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+}
+
 pub fn print_something() {
     let mut writer = Writer {
         column_position: 0,
