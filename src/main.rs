4e54f73adf8ef2fc31f6268ac1bb2563f167a31d
sugawara kensuke <superradio86@gmail.com>
2021-06-22 21:15:54 +0900

    Update main.rs to use println macro

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World!{}", "!");
 
     loop {}
 }
