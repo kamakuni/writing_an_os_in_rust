97dfaad5db569202d10107b2bcec70905220bae1
sugawara kensuke <superradio86@gmail.com>
2021-06-27 23:59:29 +0900

    Add reexport_test_harness_main attribute

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[reexport_test_harness_main = "test_main"]
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     println!("Hello World!{}", "!");
