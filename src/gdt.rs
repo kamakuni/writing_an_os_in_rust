04fa3600e06fe6b8bf6a15c14db1abc575618f5b
sugawara kensuke <superradio86@gmail.com>
2021-07-28 21:27:33 +0900

    Add GDT

--- src/gdt.rs
+++ src/gdt.rs
@@ -1,4 +1,5 @@
 use x86_64::VirtAddr;
+use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor};
 use x86_64::structures::tss::TaskStateSegment;
 use lazy_static::lazy_static;
 
@@ -16,5 +17,14 @@ lazy_static! {
             stack_end
         };
         tss
+    };
+}
+
+lazy_static! {
+    static ref GDT: GlobalDescriptorTable = {
+        let mut gdt = GlobalDescriptorTable::new();
+        gdt.add_entry(Descriptor::kernel_code_segment());   
+        gdt.add_entry(Descriptor::tss_segment(&TSS));
+        gdt
     };
 }
