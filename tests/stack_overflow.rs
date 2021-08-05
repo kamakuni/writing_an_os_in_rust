fb9746c5ac08278326384ba0c37b37d370087fa6
sugawara kensuke <superradio86@gmail.com>
2021-08-05 23:07:42 +0900

    Implement _start function

--- tests/stack_overflow.rs
+++ tests/stack_overflow.rs
@@ -5,7 +5,20 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    unimplemented!();
+    serial_print!("stack_overflow::stack_overflow...\t");
+
+    writing_an_os_in_rust::gdt::init();
+    init_test_idt();
+
+    stack_overflow();
+
+    panic!("Execution continued after stack overflow");
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow();
+    volatile::Volatile::new(0).read();
 }
 
 #[panic_handler]
